ed9d05be28e614f26f48566b1256897ec4065f57
CptMerlot <CptMerlot@gmail.com>
2022-03-17 22:57:43 -0400

    modified the drawing of enemy details

--- game/combat.py
+++ game/combat.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING, Optional, Tuple
 import pygame as pg
 from pygame.surface import Surface
 from pygame.rect import Rect
@@ -56,11 +56,12 @@ class CombatScreen():
                       line_sprite: Surface,
                       pokemon_rect: Rect,
                       detail_rect: Rect,
-                      pokemon: Pokemon):
+                      pokemon: Pokemon,
+                      skip_hp=False):
         poke = pg.transform.scale(pokemon_sprite, (pokemon_rect.width, pokemon_rect.height))
         self._screen.blit(poke, pokemon_rect.topleft)
 
-        hp_rect = self._draw_hp_detail(pokemon, detail_rect)
+        hp_rect = self._draw_hp_detail(pokemon, detail_rect, skip_hp)
         hp_bar_rect = self._draw_hp_bar_detail(pokemon, hp_rect)
         hp_level_rect = self._draw_level_detail(pokemon.level, hp_bar_rect)
         _ = self._draw_name_detail(pokemon.name, hp_level_rect)
@@ -82,60 +83,69 @@ class CombatScreen():
 
     def _draw_hp_bar_detail(self, pokemon: Pokemon, hp_rect: Rect) -> Rect:
         hp_precent = (pokemon.current_hitpoints / pokemon.hitpoints)
-        hp_bar_rect = pg.draw.rect(self._screen, (0, 0, 0), (hp_rect.centerx,
-                                   hp_rect.top-hp_rect.height, 150, int(hp_rect.height * .50)), 1)
+        hp_bar_rect = pg.draw.rect(self._screen, (0, 0, 0), (hp_rect.left,
+                                   hp_rect.top-hp_rect.height,
+                                   self.game_loop.display_info.hp_bar_width,
+                                   int(hp_rect.height * .50)), 1)
         hp_precent_color = (7, 117, 1)
         if hp_precent < 0.30:
             hp_precent_color = (255, 76, 5)
         elif hp_precent < 0.50:
             hp_precent_color = (167, 176, 2)
         pg.draw.rect(self._screen, hp_precent_color, (*hp_bar_rect.topleft,
-                                                      150*hp_precent, int(hp_rect.height * .50)), 0)
+                                                      self.game_loop.display_info.hp_bar_width * hp_precent,
+                                                      int(hp_rect.height * .50)), 0)
 
         hp_string = self.game_loop.display_info.details_font.render("HP:", False, (0, 0, 0))
         hp_string_rect = hp_string.get_rect(midright=hp_bar_rect.midleft)
         self._screen.blit(hp_string, hp_string_rect)
         return hp_bar_rect
 
-    def _draw_hp_detail(self, pokemon: Pokemon, detail_rect: Rect) -> Rect:
+    def _draw_hp_detail(self, pokemon: Pokemon, detail_rect: Rect, skip_hp: bool) -> Rect:
         hp = self.game_loop.display_info.details_font.render("{0} / {1}".format(
                                                              pokemon.current_hitpoints,
                                                              pokemon.hitpoints),
                                                              False, (0, 0, 0))
-        hp_rect = hp.get_rect(centerx=detail_rect.centerx,
-                              bottom=detail_rect.bottom - (detail_rect.height * .15))
-        self._screen.blit(hp, hp_rect)
+        hp_rect = hp.get_rect(left=(detail_rect.left +
+                                    (detail_rect.width * self.game_loop.display_info.detail_text_start_left_mod)),
+                              bottom=detail_rect.bottom -
+
+                              (detail_rect.height * self.game_loop.display_info.detail_text_start_bottom_mod))
+        if not skip_hp:
+            self._screen.blit(hp, hp_rect)
         return hp_rect
 
-    def _draw_enemy_details(self):
-        pokemon_rect = pg.rect.Rect((self._screen.get_width() - int(self._screen.get_width()/2.7),
-                                     self._screen.get_height()/20,
-                                     *self.game_loop.display_info.pokemon_size))
-        detail = pg.rect.Rect((0 + (self._screen.get_width()/50),
-                               self._screen.get_height()/20,
-                               *self.game_loop.display_info.detail_size))
-        self._draw_details(e_pokemon, eLine, pokemon_rect, detail, self._combat._computer_pokemon)
+    def _draw_enemy_details(self, player_pokemon_rect: Rect, player_detail_rect: Rect):
+        pokemon_rect = pg.rect.Rect(self._screen.get_width() - int(self._screen.get_width()/2.7),
+                                    (player_detail_rect.top - self.game_loop.display_info.pokemon_size[1]) * .50,
+                                    *self.game_loop.display_info.pokemon_size)
+        detail = pg.rect.Rect(0 + (self._screen.get_width()/50),
+                              (player_pokemon_rect.top - self.game_loop.display_info.detail_size[1])*.50,
+                              *self.game_loop.display_info.detail_size)
+        if self._combat is not None:
+            self._draw_details(e_pokemon, eLine, pokemon_rect, detail, self._combat._computer_pokemon, True)
 
-    def _draw_player_details(self, combat_screen: Rect):
+    def _draw_player_details(self, combat_txt_screen: Rect) -> Tuple[Rect, Rect]:
         # TODO: move dynamic sizing into the display_info class
 
-        pokemon_rect = pg.rect.Rect(combat_screen.left + int(combat_screen.width / 10),
-                                    ((self._screen.get_height() - combat_screen.height) -
-                                     self.game_loop.display_info.pokemon_size[1]),
+        pokemon_rect = pg.rect.Rect(combat_txt_screen.left + int(combat_txt_screen.width / 10),
+                                    ((self._screen.get_height() - combat_txt_screen.height) -
+                                    self.game_loop.display_info.pokemon_size[1]),
                                     *self.game_loop.display_info.pokemon_size)
-        detail = pg.rect.Rect((self._screen.get_width() - self._screen.get_width()/2.5) - self._screen.get_width()/50,
-                              (self._screen.get_height() - combat_screen.height) -
-                              self.game_loop.display_info.detail_size[1],
-                              *self.game_loop.display_info.detail_size)
+        detail_rect = pg.rect.Rect((self._screen.get_width() - self._screen.get_width()/2.5) - self._screen.get_width()/50,
+                                   (self._screen.get_height() - combat_txt_screen.height) -
+                                   self.game_loop.display_info.detail_size[1],
+                                   *self.game_loop.display_info.detail_size)
         if self._combat is None:
             raise BaseException("Combat has not started?")
-        self._draw_details(p_pokemon, pLine, pokemon_rect, detail, self._combat._player_pokemon)
+        self._draw_details(p_pokemon, pLine, pokemon_rect, detail_rect, self._combat._player_pokemon)
+        return pokemon_rect, detail_rect
 
-    def _draw_combat_details(self, combat_screen: Rect):
+    def _draw_combat_details(self, combat_txt_screen: Rect):
         # TODO: Have player details be created so enemy details
         # can be drawn in relation to the player rects
-        self._draw_player_details(combat_screen)
-        self._draw_enemy_details()
+        pokemon_rect, detail_rect = self._draw_player_details(combat_txt_screen)
+        self._draw_enemy_details(pokemon_rect, detail_rect)
 
     def render(self):
         if self._combat is None:
