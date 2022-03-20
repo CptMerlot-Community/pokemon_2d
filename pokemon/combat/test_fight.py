f70dee3b6016d24cbae1d3643871084f4afb4d8d
CptMerlot <CptMerlot@gmail.com>
2022-03-19 20:26:36 -0400

    added new fast computer test to make sure speed for selecting who goes first is correct

--- pokemon/combat/test_fight.py
+++ pokemon/combat/test_fight.py
@@ -6,7 +6,6 @@ from pokemon.pokemons import _load_pokemon_types
 
 @pytest.fixture
 def player_pokemon() -> Pokemon:
-
     pt = _load_pokemon_types(["grass", "rock"])
     poke = PokemonSchema(1, Name("test_pokemon"), pt,
                          Base(10, 20, 30, 30, 30, 20)
@@ -23,9 +22,21 @@ def computer_pokemon() -> Pokemon:
     return Pokemon(poke, (5, 5))
 
 
-def test_current_pokemon_logic(player_pokemon, computer_pokemon):
+@pytest.fixture
+def fast_computer_pokemon() -> Pokemon:
+    pt = _load_pokemon_types(["grass", "rock"])
+    poke = PokemonSchema(1, Name("test_pokemon"), pt,
+                         Base(10, 20, 30, 30, 30, 21)
+                         )
+    return Pokemon(poke, (6, 6))
+
+
+def test_current_pokemon_logic(player_pokemon, computer_pokemon, fast_computer_pokemon):
     c = Combat(player_pokemon, computer_pokemon)
     assert c._current_turn == c._player_pokemon
+
+    c = Combat(player_pokemon, fast_computer_pokemon)
+    assert c._current_turn == c._computer_pokemon
 
 
 def test_combat_attack_phase(player_pokemon, computer_pokemon):
